da7c807bb602f826377bc9f3805f7b111269d86f
taiwania <taiwaniajusto@gmail.com>
2023-07-03 19:13:01 +0800

    Build new.handlebars and related route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,5 +7,10 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+// Show the new expense input form
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
 // Export
 module.exports = router
