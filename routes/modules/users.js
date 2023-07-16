869d0db7cffc3ec17cc1806d19ac91aa84f100fc
taiwania <taiwaniajusto@gmail.com>
2023-07-17 00:55:41 +0800

    Set register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const User = require('../../models/user')
 
 // Show login page
@@ -14,6 +15,7 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+// Login
 router.post(
   '/login',
   passport.authenticate('local', {
@@ -22,12 +24,50 @@ router.post(
   })
 )
 
+// Logout
 router.get('/logout', function (req, res, next) {
   req.logout((error) => {
     if (error) {
       return next(error)
     }
     res.redirect('/users/login')
+  })
+})
+
+// Register
+router.post('/register', (req, res) => {
+  const { username, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', {
+        username,
+        email,
+        password,
+        confirmPassword,
+      })
+    } else if (password !== confirmPassword) {
+      console.log('Passwords do not match.')
+      res.render('register', {
+        username,
+        email,
+        password,
+        confirmPassword,
+      })
+    } else {
+      return bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hash) => {
+          User.create({
+            username,
+            email,
+            password: hash,
+          })
+        })
+        .then(() => res.redirect('/'))
+        .catch((error) => console.log(error))
+    }
   })
 })
 
