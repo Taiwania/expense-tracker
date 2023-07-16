8b5e9acc53efe9a517b7c3d218398c11b413eb19
taiwania <taiwaniajusto@gmail.com>
2023-07-16 10:34:25 +0800

    Import records to index

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,13 +2,35 @@
 const express = require('express')
 const router = express.Router()
 const Category = require('../../models/category')
+const Record = require('../../models/record')
+
+// Set category icons
+const CATEGORY = {
+  '家居物業': 'fa-home',
+  '交通出行': 'fa-shuttle-van',
+  '休閒娛樂': 'fa-grin-beam',
+  '餐飲食品': 'fa-utensils',
+  '其他': 'fa-pen',
+}
 
 // Home
 router.get('/', (req, res) => {
-  Category.find({})
-    .lean()
-    .then((categories) => res.render('index', { categories }))
-    .catch((error) => console.log(error))
+  Promise.all([
+    Category.find({}).lean(),
+    Record.find({}).populate('categoryId').lean(),
+  ])
+    .then(([categories, records]) => {
+      records.forEach((record) => {
+        let date = new Date(record.date)
+        let year = date.getFullYear()
+        let month = (date.getMonth() + 1).toString().padStart(2, '0')
+        let day = date.getDate().toString().padStart(2, '0')
+        record.date = `${year}-${month}-${day}`
+        record.categoryIcon = CATEGORY[record.categoryId.name]
+      })
+      res.render('index', { categories, records })
+    })
+    .catch(error => console.log(error))
 })
 
 // Show the new expense input form
